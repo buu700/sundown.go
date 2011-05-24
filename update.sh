cd0326851b3e933843f9e91c09d2a2b9c0a5870b
Ryan Lester <buu700@gmail.com>
2011-05-24 04:32:23 -0400

    Fixed bug in update.sh

--- update.sh
+++ update.sh
@@ -2,12 +2,12 @@
 
 rm array.c array.h buffer.c buffer.h html.c html.h html_smartypants.c markdown.c markdown.h
 
-wget https://github.com/tanoku/upskirt/raw/master/src/array.c
-wget https://github.com/tanoku/upskirt/raw/master/src/array.h
-wget https://github.com/tanoku/upskirt/raw/master/src/buffer.c
-wget https://github.com/tanoku/upskirt/raw/master/src/buffer.h
-wget https://github.com/tanoku/upskirt/raw/master/src/markdown.c
-wget https://github.com/tanoku/upskirt/raw/master/src/markdown.h
-wget https://github.com/tanoku/upskirt/raw/master/render/html.c
-wget https://github.com/tanoku/upskirt/raw/master/render/html.h
-wget https://github.com/tanoku/upskirt/raw/master/render/html_smartypants.h
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/src/array.c
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/src/array.h
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/src/buffer.c
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/src/buffer.h
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/src/markdown.c
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/src/markdown.h
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/render/html.c
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/render/html.h
+wget --no-check-certificate https://github.com/tanoku/upskirt/raw/master/render/html_smartypants.c
